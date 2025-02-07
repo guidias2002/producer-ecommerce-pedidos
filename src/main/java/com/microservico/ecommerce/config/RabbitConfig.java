034f09881a374e6ea82a94ef7b9e1579eca3ecda
guidias2002 <guidias200220@gmail.com>
2025-02-07 11:21:51 -0300

    feat: created PedidoConfig

---
+++ src/main/java/com/microservico/ecommerce/config/RabbitConfig.java
@@ -0,0 +1,25 @@
+package com.microservico.ecommerce.config;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    // Bean para conversão de mensagens JSON
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // Configuração do RabbitTemplate para garantir que envie JSON corretamente
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+}
